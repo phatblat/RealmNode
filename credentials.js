7b640f9ce9dd6ee6c27f94052408701fe6e38904
Ben Chatelain <ben@octop.ad>
2017-08-21 12:53:54 -0600

    Add credentials and objectServerAuth modules

---
+++ credentials.js
@@ -0,0 +1,7 @@
+module.exports = {
+    user: 'test@log-g.co',
+    password: 'password',
+    server: 'http://127.0.0.1:9080',
+    questserver: 'realm://127.0.0.1:9080/qna-question-realm',
+    eventserver: 'realm://127.0.0.1:9080/qna-event-realm',
+}
---
+++ objectServerAuth.js
@@ -0,0 +1,4 @@
+
+const credentials = require('./credentials.js')
+const debug = require('debug')
+const Realm = require('realm')
